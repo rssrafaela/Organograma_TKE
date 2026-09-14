27fe94ae06cd9674eb662c9e6f107dca338aefdb
Rafaela Souza <130423920+rssrafaela@users.noreply.github.com>
2026-09-14 15:01:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll(".slide-btn");
+const pages = document.querySelectorAll(".page");
+
+buttons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+
+        const pageId = button.dataset.page;
+
+        buttons.forEach((item) => {
+            item.classList.remove("active");
+        });
+
+        pages.forEach((page) => {
+            page.classList.remove("active");
+        });
+
+        button.classList.add("active");
+
+        document
+            .getElementById(pageId)
+            .classList.add("active");
+
+    });
+
+});
